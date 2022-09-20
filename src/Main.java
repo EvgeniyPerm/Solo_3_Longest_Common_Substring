2474aef631c6ff827dd788a90f2fb62beec88622
EvgeniyPerm <112462287+EvgeniyPerm@users.noreply.github.com>
2022-09-20 15:18:31 +0500

    Task: Given multiple words, you need to find the longest string that is a substring of all words.
    Input Format:
    A string of words, separated by spaces. The string can also contain numbers.
    
    Output Format:
    A string, representing the longest common substring.
    If there are multiple longest common substrings, output the smallest one in alphabetical order.
    
    Sample Input:
    SoloLearn Learning LearningIsFun Learnable
    
    Sample Output:
    Learn

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
+import java.util.*;
+
 public class Main {
+    public static String[] strings;
+    public static String[] result = new String[100];
+    public static int iResult = 0;
+    public static String findString(int iNeed, int cnt){
+        iResult = 0;
+        for (int j = 0; j < strings[iNeed].length()-cnt+1; j++) {
+            String current = strings[iNeed].substring(j,j+cnt);
+            boolean isEverywere = true;
+            for (int i = 0; i < strings.length; i++) {
+                if (strings[i].indexOf(current) == -1) {
+                    isEverywere = false;
+                    break;
+                };
+            };
+            if (isEverywere)  {
+                result[iResult++] = current;
+            };
+
+        };
+        if (iResult==0) return null;
+        String min =result[0] ;
+        for (int i = 0; i < iResult; i++) {
+            if (result[i].compareTo(min)<0) min = result[i];
+        }
+        return min;
+
+
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        strings = sc.nextLine().trim().split(" ");
+//        strings = "d234fqwere qwer!234 s234dfqwerdg gqwe234rh f234qwefggs werqwer32234rwe".split(" ");
+        int iMin = 0;
+        for (int i = 0; i < strings.length; i++) {
+            if (strings[i].length()< strings[iMin].length()) iMin=i;
+        };
+        String result;
+        for (int i = strings[iMin].length(); i > 0; i--) {
+            result = findString(iMin,i);
+            if (result!=null){
+                System.out.println(result);
+                break;
+            }
+
+        }
+
     }
 }
